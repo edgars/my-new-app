a2eb1e84f491af7c2ad03ab1b9e7c61b1a41144d
edgars <edgar@skalena.com>
2026-06-16 21:09:03 -0300

    chore: clean repo for fresh project

--- lib/rules/BR-005.ts
+++
@@ -1,3 +0,0 @@
-// TODO(rnc): implement business rule BR-005 (budget limit reached)
-// Procedure: TBrPartsForm.EditBtnClick
-// condition: procedure TBrPartsForm.EditBtnClick(Sender:TObject)
--- lib/rules/BR-008.ts
+++
@@ -1,3 +0,0 @@
-// TODO(rnc): implement business rule BR-008 (budget limit reached)
-// Procedure: TBrPartsForm.FormShow
-// condition: procedure TBrPartsForm.FormShow(Sender:TObject)
