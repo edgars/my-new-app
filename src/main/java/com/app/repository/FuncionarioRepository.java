a9e1d182d3138a56651b4a6164af3f1f11145a39
edgars <edgar@skalena.com>
2026-06-26 16:18:05 -0300

    chore: clean repo for fresh project

--- src/main/java/com/app/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.repository;
-
-import com.app.entity.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
--- src/main/java/com/app/repository/LotacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.repository;
-
-import com.app.entity.Lotacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LotacaoRepository extends JpaRepository<Lotacao, Long> {
-}
